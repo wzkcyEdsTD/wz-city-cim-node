4d8e4713b4bcf5597a45394ac7812d6e364ead3e
EdisonForMan <chuanghewzh12@hotmail.com>
2020-11-20 17:33:40 +0800

    update

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const port = 3000;
 const bodyParser = require("body-parser");
 const { conn } = require("./oracle/oracle");
 const router = require("./router/index.js");
-//  server
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-//  static
-app.use("/cim", express.static(__dirname + "/cim"));
-//  router
-app.use("/", router);
 //  oracle
-conn();
-//  listen
-app.listen(port, () => console.log("server on 3000"));
+conn(() => {
+    //  server
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    //  static
+    app.use("/cim", express.static(__dirname + "/cim"));
+    app.use("/static", express.static(__dirname + "/cim/static"));
+    //  router
+    app.use("/", router);
+    //  listen
+    app.listen(port, () => console.log("server on 3000"));
+});
--- oracle/oracle.js
+++ oracle/oracle.js
@@ -3,13 +3,14 @@ const oracledb = require("oracledb");
 const option = { resultSet: false, outFormat: oracledb.OBJECT };
 //  oracle
 let oraConnection;
-const conn = () => {
+const conn = (fn) => {
   oracledb.getConnection(oraConfig, (err, connection) => {
     if (err) {
       console.log("Fail to connect oracle:", err);
       return;
     }
     oraConnection = connection;
+    fn && fn();
   });
 };
 /**
@@ -17,11 +18,11 @@ const conn = () => {
  * @param {*} SQL 
  * @param {*} fn 
  */
-const oracleExecute = (SQL) => {
+const oracleExecute = (SQL, data = []) => {
   return new Promise((resolve, reject) => {
     oraConnection.execute(
       SQL,
-      [],
+      data,
       option,
       (err, result) => {
         err ? reject(err.message) : resolve(result)
@@ -35,69 +36,76 @@ const oracleExecute = (SQL) => {
  * @param {*} userid
  * @param {*} fn
  */
-const getEventList = async (data) => {
+const getEventList = async (params) => {
   return oracleExecute(
-    "SELECT OCCURORG,SUBJECT,LASTUSERNAME,LAT,LON,OCCURDATE FROM ISSUES_TEMP ORDER BY OCCURDATE DESC",
+    "SELECT DISTINCT ID,OCCURORG,SUBJECT,LASTUSERNAME,LAT,LON,OCCURDATE,ISSUECONTENT FROM ISSUES_TEMP ORDER BY OCCURDATE DESC",
   );
 };
+const getEventLog = async (params) => {
+  const { id } = params;
+  return oracleExecute(
+    "SELECT DISTINCT DEALUSERNAME,DEALDESCRIPTION,DEALTIME FROM ISSUELOG_TEMP WHERE ISSUEID = :id ORDER BY DEALTIME ASC",
+    [id]
+  );
+}
 /**
  * 获取刑满释放人员
- * @param {*} data 
+ * @param {*} params 
  * @param {*} fn 
  */
-const getPositiveinfosList = (data) => {
+const getPositiveinfosList = (params) => {
   return oracleExecute(
-    "SELECT ORGID,NATIVEPOLICESTATION,CURRENTADDRESS,NAME,RELEASEORBACKDATE FROM POSITIVEINFOS_TEMP ORDER BY RELEASEORBACKDATE DESC",
+    "SELECT DISTINCT ORGID,NATIVEPOLICESTATION,CURRENTADDRESS,NAME,RELEASEORBACKDATE FROM POSITIVEINFOS_TEMP ORDER BY RELEASEORBACKDATE DESC",
   );
 };
 /**
  * 获取社区矫正人员
- * @param {*} data 
+ * @param {*} params 
  * @param {*} fn 
  */
-const getRectificativepersonsList = (data) => {
+const getRectificativepersonsList = (params) => {
   return oracleExecute(
-    "SELECT ORGID,NATIVEPOLICESTATION,CURRENTADDRESS,NAME FROM RECTIFICATIVEPERSONS_TEMP ORDER BY ORGID ASC",
+    "SELECT DISTINCT ORGID,NATIVEPOLICESTATION,CURRENTADDRESS,NAME FROM RECTIFICATIVEPERSONS_TEMP ORDER BY ORGID ASC",
   );
 };
 /**
  * 获取精神病人员
- * @param {*} data 
+ * @param {*} params 
  * @param {*} fn 
  */
-const getMentalpatientsList = (data) => {
+const getMentalpatientsList = (params) => {
   return oracleExecute(
-    "SELECT ORGID,NATIVEPOLICESTATION,NATIVEPLACEADDRESS,NAME FROM MENTALPATIENTS_TEMP ORDER BY ORGID ASC",
+    "SELECT DISTINCT ORGID,NATIVEPOLICESTATION,NATIVEPLACEADDRESS,NAME FROM MENTALPATIENTS_TEMP ORDER BY ORGID ASC",
   );
 };
 /**
  * 获取吸毒人员
- * @param {*} data 
+ * @param {*} params 
  * @param {*} fn 
  */
-const getDruggysList = (data) => {
+const getDruggysList = (params) => {
   return oracleExecute(
-    "SELECT ORGID,NATIVEPOLICESTATION,CURRENTADDRESS,NAME FROM DRUGGYS_TEMP ORDER BY ORGID ASC",
+    "SELECT DISTINCT ORGID,NATIVEPOLICESTATION,CURRENTADDRESS,NAME FROM DRUGGYS_TEMP ORDER BY ORGID ASC",
   );
 };
 /**
  * 获取重点上访人员
- * @param {*} data 
+ * @param {*} params 
  * @param {*} fn 
  */
-const getSuperiorvisitsList = (data) => {
+const getSuperiorvisitsList = (params) => {
   return oracleExecute(
-    "SELECT ORGID,NATIVEPOLICESTATION,CURRENTADDRESS,NAME FROM SUPERIORVISITS_TEMP ORDER BY ORGID ASC",
+    "SELECT DISTINCT ORGID,NATIVEPOLICESTATION,CURRENTADDRESS,NAME FROM SUPERIORVISITS_TEMP ORDER BY ORGID ASC",
   );
 };
 /**
  * 获取涉稳人员信息
- * @param {*} data 
+ * @param {*} params 
  * @param {*} fn 
  */
-const getInvolvingstabilitypeList = (data) => {
+const getInvolvingstabilitypeList = (params) => {
   return oracleExecute(
-    "SELECT ORGID,NATIVEPOLICESTATION,CURRENTADDRESS,NAME FROM INVOLVINGSTABILITYPS_TEMP ORDER BY ORGID ASC",
+    "SELECT DISTINCT ORGID,NATIVEPOLICESTATION,CURRENTADDRESS,NAME FROM INVOLVINGSTABILITYPS_TEMP ORDER BY ORGID ASC",
   );
 };
 /**
@@ -107,6 +115,7 @@ const doRelease = () => oraConnection.close();
 module.exports = {
   conn, doRelease,
   getEventList,
+  getEventLog,
   getPositiveinfosList,
   getRectificativepersonsList,
   getMentalpatientsList,
