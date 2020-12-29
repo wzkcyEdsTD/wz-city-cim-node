4f477438f402ffdff22559550f36d0986eaac12c
EdisonForMan <chuanghewzh12@hotmail.com>
2020-12-29 15:20:57 +0800

    根据网格id获取网格管理信息

--- router/grid/index.js
+++ router/grid/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const {
     getGridMemberList,
-    getGridMemberRouteLink
+    getGridMemberRouteLink,
+    getGridManagerByGrid
 } = require("../../oracle/oracle");
 //  获取网格员列表
 router.get("/getGridMemberList", async (req, res) => {
@@ -12,6 +13,10 @@ router.get("/getGridMemberList", async (req, res) => {
 //  获取网格员行动轨迹
 router.get("/getGridMemberRouteLink", async (req, res) => {
     const data = await getGridMemberRouteLink(req.query)
+    res.send({ status: 200, data });
+})
+router.get("/getGridManagerByGrid", async (req, res) => {
+    const data = await getGridManagerByGrid(req.query)
     res.send({ status: 200, data });
 })
 module.exports = router;
